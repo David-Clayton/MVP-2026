23f9f7cd74e23b27aef2fe2110656c187ce937c8
David-Clayton <s2413476@ed.ac.uk>
2026-03-18 16:23:34 +0000

    Started Poisson

---
+++ Checkpoint 3/Checkpoint 3 Poisson.py
@@ -0,0 +1,11 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.animation import FuncAnimation as animate
+import argparse
+import time
+import random
+from numba import njit
+from joblib import Parallel, delayed
+
+class Poisson:
+    """"""
