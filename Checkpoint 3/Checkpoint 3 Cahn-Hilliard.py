b1258fc6699a1af9553d30b7afc0312cfc165cc7
David-Clayton <s2413476@ed.ac.uk>
2026-03-18 15:37:39 +0000

    Free energy evolution plots for Cahn-Hilliard

--- Checkpoint 3/Checkpoint 3 Cahn-Hilliard.py
+++ Checkpoint 3/Checkpoint 3 Cahn-Hilliard.py
@@ -18,6 +18,7 @@ class CahnHilliard:
         self.space_step = space_step
         self.time_step = time_step
         self.parameter = parameter
+        self.phi_0 = phi_0
 
     def calc_chem_pot(self):
         """Calculate the (dimensionless) chemical potential (mu) across the lattice
@@ -76,12 +77,62 @@ class CahnHilliard:
         plt.show()
         return animation
 
+    def calculate_free_energy(self):
+        """Calculate the free energy density (f) of the system at a
+        single time step at order parameter phi"""
+
+        #Neighbours of each phi lattice point
+        phi = self.lattice
+        phi_left = np.roll(self.lattice, shift = 1, axis = 1)
+        phi_right = np.roll(self.lattice, shift = -1, axis = 1)
+        phi_up = np.roll(self.lattice, shift = 1, axis = 0)
+        phi_down = np.roll(self.lattice, shift = -1, axis = 0)
+
+        #Calculate f with discretised free energy equation
+        f = -0.5*(phi**2) + 0.25*(phi**4) + ((self.parameter/2)*(phi_left**2 + phi_down**2 + 2*phi**2 - 2*phi_left*phi - 2*phi_down*phi)/self.space_step**2)
+
+        return f
+    
+    def free_energy_plot(self, no_meas = 2000, iter_per_meas = 1000):
+        """Plot the evolution of free energy of the system over time"""
+        #Array of timesteps
+        time_data = self.time_step * np.linspace(0, no_meas*iter_per_meas, no_meas)
+        #Initialise empty array for free energy data
+        f_data = np.zeros(no_meas)
+
+        for i in range(no_meas):
+            for j in range(iter_per_meas):
+                self.calc_order_param()
+            f = self.calculate_free_energy()
+            f_data[i] = np.mean(f)
+            if i % 100 == 0:
+                print(f"Free energy computed for measurement {i}/{no_meas}.")
+
+        plt.plot(time_data, f_data, markersize = 2, marker = "o", color = "orange")
+        plt.grid(which = "both")
+        plt.xlabel(r"Time", fontsize = 16)
+        plt.ylabel(r"Mean free energy density (f)", fontsize = 16)
+        plt.title(f"Time evolution of free energy density for $\\phi_0$ = {self.phi_0}", fontsize = 12)
+        plt.tick_params(axis = "both", labelsize = 12)
+        plt.tight_layout()
+        plt.savefig(f"Free_energy_phi_0.0.png")
+        plt.show()
+
+        data = np.column_stack((time_data, f_data))
+        np.savetxt("Free_energy_0.0_csv", data, delimiter=",", header = "time, f")
+    
+
+
+            
+
+
 
 def main():
-    c = CahnHilliard(phi_0=0.5, size=100, parameter=1, space_step=1, time_step=0.025)
-    c.animate_lattice()
-
-    
+    time_0 = time.time()
+    c = CahnHilliard(phi_0=0.0, size=100, parameter=1, space_step=1, time_step=0.02)
+    c.free_energy_plot()
+    time_1 = time.time()
+    print(f"Functions done in {(time_1 - time_0)/60} minutes")
 main()
 
 
